ce6026ee99c7a7548bb78e320827c65f3c104273
suhail sullad <suhail-sullad@gmail.com>
2014-12-05 10:09:11 +0530

    updated project structure

---
+++ src/test/java/mobiletests/MobileTest.java
@@ -0,0 +1,5 @@
+package mobiletests;
+
+public class MobileTest {
+
+}
---
+++ src/test/java/uitests/ScreenRecorderHook.java
@@ -0,0 +1,68 @@
+package uitests;
+import initilization.DriverManager;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import logging.WebDriverListner;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import screenrecording.RecordScreen;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class ScreenRecorderHook{
+	private RecordScreen record;
+	protected final EventFiringWebDriver fd;
+	private String videoreporting;
+	private WebDriverListner listner;
+	private String loggingstatus;
+	public ScreenRecorderHook(DriverManager webdriver) throws IOException{
+		Properties prop = new Properties();
+		FileInputStream input = new FileInputStream("./tests.properties");
+		prop.load(input);
+		this.fd=webdriver;
+		this.videoreporting=prop.getProperty("videoreporting");
+		this.loggingstatus=prop.getProperty("logging");
+		
+	}
+	@Before
+	public void preparevideo(Scenario scenario) throws Exception {
+		
+		
+		if(videoreporting.equalsIgnoreCase("ENABLED"))
+		{
+			record = new RecordScreen("testrecording");
+			record.beginrecord();
+			Thread.sleep(1000);
+		}
+	
+		if(loggingstatus.equalsIgnoreCase("ENABLED"))
+		{
+		listner = new WebDriverListner(scenario.getName());
+		fd.register(listner);
+		}
+
+	}
+	@After
+	public void close(Scenario scenario) throws IOException,
+			InterruptedException {
+		scenario.embed(this.fd.getScreenshotAs(OutputType.BYTES), "image/png");
+		if(videoreporting.equalsIgnoreCase("ENABLED"))
+		scenario.write("<br/><a href=\"" + record.stoprecording()+ "\">Click here to navigate to video</a>");
+		this.fd.quit();
+		if(loggingstatus.equalsIgnoreCase("ENABLED"))
+		{
+			listner.write("Browser closed.");
+			listner.writescenariosttus(scenario.getName(),scenario.getStatus());
+			listner.quitlogger();
+			scenario.write("<br/><a href=\"../logs/" +scenario.getName()+ ".html\">Click for log information.</a>");
+		}
+		
+	}
+
+}
